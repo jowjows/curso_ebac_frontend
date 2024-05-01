0fce1ba2acec5c39229e81be850a78bb42a38bb3
jowjows <raissarochesso@gmail.com>
2024-05-01 14:39:09 -0300

    atualização css

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 const form = document.getElementById("form-ab");
+const campoA = document.getElementById('campo-a');
+const campoB = document.getElementById('campo-b');
 
 function validaCampo(a,b){
     return b > a
@@ -8,21 +10,52 @@ form.addEventListener('submit',function(e){
     let formValido = false;
     e.preventDefault();
 
-    const campoA = document.getElementById('campo-a');
-    const campoB = document.getElementById('campo-b');
+
+    const messagemSucesso = 'Formulário válido.';
+    const menssagemErro = 'O valor de B precisa ser MAIOR que o valor de A.';
+
 
     formValido = validaCampo(campoA.value,campoB.value);
 
 
     if (formValido){
-        alert('Formulário válido');
-        console.log(formValido);
+        const containerMensagem = document.querySelector(".sucesso");
+        containerMensagem.innerHTML = messagemSucesso;
+        containerMensagem.style.display = 'block';
+        const containerMensagemErro = document.querySelector(".erro");
+        containerMensagemErro.style.display = 'none'
+
 
         campoA.value = '';
         campoB.value = '';
     }else{
-        alert('Formulário inválido');
+        const containerMensagem = document.querySelector(".erro");
+        containerMensagem.innerHTML = menssagemErro;
+        containerMensagem.style.display = 'block';
+
+        const containerMensagemSucesso = document.querySelector(".sucesso");
+        containerMensagemSucesso.style.display = 'none'
+
+        campoA.value = '';
+        campoB.value = '';
+
     }
+
+
+})
+
+
+form.addEventListener('reset',function(e){
+    let formValido = false;
+    e.preventDefault();
+    const containerMensagemErro = document.querySelector(".erro");
+    containerMensagemErro.style.display = 'none'
+
+    const containerMensagemSucesso = document.querySelector(".sucesso");
+    containerMensagemSucesso.style.display = 'none'
+
+    campoA.value = '';
+    campoB.value = '';
 
 
 })
